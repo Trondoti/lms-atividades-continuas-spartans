d38895a0826678e69186f14dda05fe564c3b1897
pablo ferreira <pabloferr92@gmail.com>
2018-04-10 00:21:15 -0300

    Realizada implementação de validação inteligente no formulario_matricula

--- docs/script_2.js
+++ docs/script_2.js
@@ -1,35 +1,41 @@
 function validateForm() {
+   
     var inputs = document.querySelectorAll('input[type="radio"]');
     var cont_aprovado = 0;
     var cont_cancelado = 0;
     var has_alerted = false;
     console.log(inputs);
 
-    for(var i = 0; i < inputs.length; i++) {
+    for(var i = 0; i < inputs.length; i++)
+    {
         if(inputs[i].checked == true && inputs[i].className == "aprovado") {
             cont_aprovado++;
-            console.log("APROVADO");
         }
         else if(inputs[i].checked == true && inputs[i].className == "cancelado") {
             cont_cancelado++;
-            console.log("CANCELADO");
         }
-        else if(inputs[i].checked == false && inputs[i].className == "aprovado" && has_alerted == false || inputs[i].checked == false && inputs[i].className == "cancelado" && has_alerted == false) {
-            console.log("Você deve preencher pelo menos uma opção em cada linha da lista.");
-            has_alerted = true;
-        }
+          
     }
-
-
-    if(cont_aprovado <= 20) {
-        alert("Mínimo de 20 alunos aprovados!")
+    if (cont_aprovado+cont_cancelado<80)
+    {
+        event.preventDefault();
+        alert("Favor preencher todas as opcoes");    
+    }
+    else if(cont_aprovado < 20)
+    {
+        event.preventDefault();
+        alert("Mínimo de 20 alunos aprovados!");
         return false;
     }
-    else if(cont_aprovado > 60) {
+    else if(cont_aprovado > 60)
+    {
+        event.preventDefault();
         alert("Máximo de 60 alunos aprovados!");
         return false;
     }
-    else {
-        console.log("Já eras");
+    else
+    {
+        alert("Parabéns ! Formulário enviado com sucesso");
     }
-}
+    
+}  

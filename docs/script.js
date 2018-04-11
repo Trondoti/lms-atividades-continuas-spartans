47fc2b3bcee1610e371233eb463525af911a4075
pablo ferreira <pabloferr92@gmail.com>
2018-04-11 18:04:13 -0300

    adicionado validacao inteligente formulario novo_aluno

--- docs/script.js
+++ docs/script.js
@@ -1,95 +1,56 @@
-document.getElementById('login').required = true;
-document.getElementById('nome').required = true;
-document.getElementById('email').required = true;
-document.getElementById('nascimento').required = true;
-document.getElementById('cpf').required = true;
-document.getElementById('senha').required = true;
-document.getElementById('confirma_senha').required = true;
+var form = document.getElementById("formulario");
+form.addEventListener("submit", validateForm);
 
-function validarSenha() {
-    var senha = document.getElementById("senha").value;
-    var aux = document.getElementById("senha");
-
-    if(senha.search(/[a-zA-Z]/) <= 0 && senha.search(/\d/) <= 0) {
-        aux.setCustomValidity("A senha deve ter letras e números!");
+function validateForm(event) {
+  //CAMPOS OBRIGATÓRIOS
+  var fields = document.getElementsByClassName("obrigatorio");
+  //console.log(fields);
+  for (var i = 0; i < fields.length; i++) {
+    if (fields[i].value == "") {
+      event.preventDefault();
+      alert("Preenchimento do campo " + fields[i].name + " obrigatório.");
     }
-    else {
-        aux.setCustomValidity("");
   }
-}
 
+  //VALIDAR SENHA
+  var password = document.getElementById("senha").value;
+  if (password.search(/[a-zA-Z]/) <= 0 && password.search(/\d/) <= 0) {
+    event.preventDefault();
+    alert("Senha inválida. Deve possuir ao menos uma letra e um número!");
+  }
 
-function senhasIguais(confirma_senha) {
-    if(confirma_senha.value != document.getElementById("senha").value) {
-        confirma_senha.setCustomValidity("As senhas devem ser iguais!");
+  //VERIFICAR SE AS SENHAS SÃO IGUAIS
+  var confirm_password = document.getElementById("confirma_senha");
+  if (confirm_password.value != document.getElementById("senha").value) {
+    event.preventDefault();
+    alert("Senhas devem ser iguais.");
+  }
+
+  //VALIDAÇÃO CPF
+  var cpf = document.getElementById("cpf").value;
+  var numeros, digitos, soma, i, resultado, digitos_iguais;
+  digitos_iguais = 1;
+  if (cpf.length < 11) {
+    alert("CPF deve ter 11 caracteres!");
+    return false;
+  }
+  for (i = 0; i < cpf.length - 1; i++)
+    if (cpf.charAt(i) != cpf.charAt(i + 1)) {
+      digitos_iguais = 0;
+      break;
     }
-    else {
-        confirma_senha.setCustomValidity("");
-    }
-}
-
-
-function validarCPF() {
-    if (vercpf(document.frmcpf.cpf.value)) {
-        console.log("Sucesso");
-    } else {
-        errors = "1";
-        if (errors) alert('CPF Inválido.');
-        document.retorno = (errors == '');
-    }
-}
-
-
-function validarIdade() {
-    var data_nascimento = document.getElementById("nascimento").value;
-    var ano_nascimento = data_nascimento.split("-")[0];
-    var mes_nascimento = data_nascimento.split("-")[1];
-    var dia_nascimento = data_nascimento.split("-")[2];
-    var data_atual = new Date();
-    var ano_atual = data_atual.getFullYear();
-    var mes_atual = data_atual.getMonth()+1;
-    var dia_atual = data_atual.getDate();
-    var ano_subtracao = ano_atual - ano_nascimento;
-    var mes_subtracao = 0;
-    var dia_subtracao = 0;
-    var idade = ano_subtracao;
-
-    if(mes_nascimento != mes_atual) {
-        if(mes_atual < mes_nascimento) {
-            idade--;
-        }
-    }
-    else if(dia_atual < dia_nascimento) {
-        idade--;
-    }
-
-    (idade <= 17) ? alert("Menores de 17 anos não podem ser cadastrados!") : console.log("Só alegria!");
-
-    console.log(idade)
-    //alert(ano_subtracao);
-    // alert(dia_atual + "/" + mes_atual + "/" + ano_atual);
-    // alert(dia_nascimento + "/" + mes_nascimento + "/" + ano_nascimento);
-}
-
-
-function vercpf(cpf) {
-    if (cpf.length != 11 || cpf == "00000000000" || cpf == "11111111111" || cpf == "22222222222" || cpf == "33333333333" || cpf == "44444444444" || cpf == "55555555555" || cpf == "66666666666" || cpf == "77777777777" || cpf == "88888888888" || cpf == "99999999999")
-        return false;
-    add = 0;
-    for (i = 0; i < 9; i++)
-        add += parseInt(cpf.charAt(i)) * (10 - i);
-    rev = 11 - (add % 11);
-    if (rev == 10 || rev == 11)
-        rev = 0;
-    if (rev != parseInt(cpf.charAt(9)))
-        return false;
-    add = 0;
-    for (i = 0; i < 10; i++)
-        add += parseInt(cpf.charAt(i)) * (11 - i);
-    rev = 11 - (add % 11);
-    if (rev == 10 || rev == 11)
-        rev = 0;
-    if (rev != parseInt(cpf.charAt(10)))
-        return false;
+  if (!digitos_iguais) {
+    numeros = cpf.substring(0, 9);
+    digitos = cpf.substring(9);
+    soma = 0;
+    for (i = 10; i > 1; i--) soma += numeros.charAt(10 - i) * i;
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(0)) { alert("CPF Inválido!"); return false; }
+    numeros = cpf.substring(0, 10);
+    soma = 0;
+    for (i = 11; i > 1; i--) soma += numeros.charAt(11 - i) * i;
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(1)) { alert("CPF Inválido!"); return false; }
     return true;
+  } else { alert("CPF Inválido!"); return false; }
 }

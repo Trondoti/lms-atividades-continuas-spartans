767932c236b90ef60d6d68118b06b5f45ba12463
pabloferr92 <pabloferr92@gmail.com>
2018-05-13 09:15:50 -0300

    Atualizar solicitacao

--- LMS/lms/settings.py
+++ LMS/lms/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'ENGINE': 'sql_server.pyodbc',
     'NAME': 'LMS',
     'USER': 'sa',
-    'PASSWORD': 'abc321',
-    'HOST': 'DESKTOP-9SF2B7V',
+    'PASSWORD': 'P@ssword1',
+    'HOST': 'PABLOFE-PC\SQLEXPRESS',
     'PORT': '',
 
     'OPTIONS': {
